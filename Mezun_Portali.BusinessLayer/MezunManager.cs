0a14c92774b4e7a2b2534a1b43f757f4f4526013
emrecavdar <emreavdar@gmail.com>
2018-12-19 00:35:57 +0300

    veritabanı veri çekme

--- Mezun_Portali.BusinessLayer/MezunManager.cs
+++ Mezun_Portali.BusinessLayer/MezunManager.cs
@@ -1,5 +1,8 @@
 ﻿using Mezun_Portali.BusinessLayer.Abstract;
+using Mezun_Portali.BusinessLayer.Results;
 using Mezun_Portali.Ent;
+using Mezun_Portali.Ent.Messages;
+using Mezun_Portali.Ent.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,36 @@ namespace Mezun_Portali.BusinessLayer
 {
     public class MezunManager:ManagerBase<MezunTablo>
     {
+        public BusinessLayerResult<MezunTablo> Kayit(RegisterViewModel data)
+        {
+            MezunTablo mezun = Find(x => x.Kullanici_Adi == data.Kullanici_Adi || x.Mail == data.Mail);
+            BusinessLayerResult<MezunTablo> res = new BusinessLayerResult<MezunTablo>();
+            if (mezun != null)
+            {
+                if (mezun.Kullanici_Adi==data.Kullanici_Adi)
+                {
+                    res.AddError(ErrorMessageCode.UsernameAlreadyExists, "Kullanıcı adı kayıtlı");
+                }
+
+                if (mezun.Kullanici_Adi==data.Kullanici_Adi)
+                {
+                    res.AddError(ErrorMessageCode.EmailAlreadyExists, "E-Posta adresi kayıtlı.");
+                }
+                else
+                {
+                    int dbResult = base.Insert(new MezunTablo()
+                    {
+                        Ad = data.Ad,
+                        Kullanici_Adi = data.Kullanici_Adi,
+                        Sifre=data.Sifre,
+                        Onaylandi=false,
+                        Mail=data.Mail
+                        
+
+                    });
+                }
+            }
+        }
 
     }
 }
